265af4bf7f1f74a3b795c238bc5a902d856db841
nekonako <ne.nekonako@gmail.com>
2023-03-23 22:11:24 +0700

    imporve top nav bar

--- kita/src/components/top_nav/top_nav.tsx
+++ kita/src/components/top_nav/top_nav.tsx
@@ -1,10 +1,21 @@
+import { IconBrandGithub } from "@tabler/icons-react"
+import Link from "next/link"
+
 export const TopNav = () => {
 
     return (
         <div className="sticky relative top-0 z-10 h-16 bg-secondary overflow-hidden">
-            <div className="flex flex-row justify-center items-center h-full">
+            <div className="flex flex-row justify-between h-full mx-24 items-center">
                 <div>
-                    test
+                    MoeDB
+                </div>
+                <div>
+                    <Link href="https://github.com/nekonako/moedb">
+                        <div className="flex flex-row space-x-4">
+                            <IconBrandGithub />
+                            <span>github</span>
+                        </div>
+                    </Link>
                 </div>
             </div>
         </div>
